d8ac1f312dc6f0788e152503840da56ca926c312
GarryCan76 <95485005+GarryCan76@users.noreply.github.com>
2023-03-22 11:28:46 +0100

    ok

---
+++ TestimonialPlugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: TestimonialPlugin
+ * Plugin URI: gluwebURL
+ * Description: Write stuff about people
+Version: 1.0.0
+Author: Krijn and Garon
+Author URI: gluwebURL
+License: GPL-2.0+
+ */
