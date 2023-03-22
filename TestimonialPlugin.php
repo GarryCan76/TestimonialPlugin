e80454b4e81e92cf3a66cd871edbb44a7d250f40
GarryCan76 <95485005+GarryCan76@users.noreply.github.com>
2023-03-22 13:50:43 +0100

    ok

--- TestimonialPlugin.php
+++ TestimonialPlugin.php
@@ -9,7 +9,32 @@ Author URI: gluwebURL
 License: GPL-2.0+
  */
 
+// Voeg html toe onderin de pagina.
 add_action('get_footer','mybox');
 function mybox(){
-    echo '<div style="background-color: aqua; width: 100px; height: 100px"></div>';
+    echo '<div class="infobox">Hier komt wat informatie</div>';
+}
+// Voeg styling toe in de <head>.
+add_action('get_header','mystyles');
+function mystyles(){
+    echo '<style>
+  .infobox{
+     background-color:black;
+   width:100%;
+   height:54px;
+   color:white;
+   display:flex;
+   justify-content:center;
+   align-items:center;
+ }
+ </style>';
+}
+
+add_action('admin_menu', 'mijnplugin_menu');
+function mijnplugin_menu(){
+    add_menu_page('Wijzig tekst','Eigen plugin','manage_options','eigenplugin_settings_page','mijnplugin_page');
+}
+function mijnplugin_page(){
+    echo '<h2>'.__('Testimonial Text','menu-test').'</h2>';
+    include_once('TestimonialPlugin_settings.php');
 }
